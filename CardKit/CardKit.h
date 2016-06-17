6f8f9412716934af1f8fbbbc952d83bea2dbdb42
Saad Ismail <saad@us.ibm.com>
2016-06-17 15:19:13 -0400

    fixed build issues

--- CardKit/CardKit.h
+++ CardKit/CardKit.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 IBM. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for CardKit.
 FOUNDATION_EXPORT double CardKitVersionNumber;
